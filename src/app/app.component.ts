529cd94f5367013342eb071217745fddb6cf639c
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-03-21 22:20:19 -0300

    Added Login Module Routing and Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,11 @@
 import { Component } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+   <p>App Compopnent</p>
+   <app-login></app-login>
+  `,
 })
-export class AppComponent {
-
-  constructor(
-    private apollo: Apollo,
-  ) {
-    this.allUsers();
-    // this.createUser();
-  }
-
-  allUsers(): void {
-    this.apollo.query({
-      query: gql`
-        query {
-          allUsers {
-            id
-            name
-            email
-          }
-        }
-      `
-    }).subscribe(res => console.log('Query', res));
-  }
-
-  createUser(): void {
-
-    this.apollo.mutate({
-      mutation: gql`
-        mutation CreateNewUser($name: String!, $email: String!, $password: String!) {
-          createUser(name: $name, email: $email, password: $password) {
-            id
-            name
-            email
-          }
-        }
-      `,
-      variables: {
-        name: 'Robert Plant',
-        email: 'robert.plant@gmail.com',
-        password: '123456789'
-      }
-    }).subscribe(res => console.log('Mutation: ', res));
-  }
-
-}
+export class AppComponent {}
