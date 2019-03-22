d6bb437cf3d58db5ac1b3297f6a371602744c26c
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-03-22 07:18:27 -0300

    Added Chat List and Users List Menu Navigation Component

--- src/app/chat/components/chat-tab/chat-tab.component.ts
+++ src/app/chat/components/chat-tab/chat-tab.component.ts
@@ -4,9 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-chat-tab',
   template: `
     <nav mat-tab-nav-bar backgroundColor="primary">
-      <a mat-tab-link>Chats</a>
-      <a mat-tab-link>Users</a>
+      <a mat-tab-link
+         routerLink="./"
+         routerLinkActive
+         #chatsRla="routerLinkActive"
+         [routerLinkActiveOptions]="{exact: true}"
+         [active]="chatsRla.isActive">
+        Chats
+      </a>
+      <a mat-tab-link
+         routerLink="users"
+         routerLinkActive
+         #usersRla="routerLinkActive"
+         [routerLinkActiveOptions]="{exact: true}"
+         [active]="usersRla.isActive">
+        Users
+      </a>
     </nav>
+    <router-outlet></router-outlet>
   `,
 })
 export class ChatTabComponent {}
