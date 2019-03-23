1eee24ccaf5d491c9961f670cc5f9777bc1ff6d7
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-03-23 00:04:41 -0300

    Added Get User by ID Method on user service

--- src/app/core/services/user.graphql.ts
+++ src/app/core/services/user.graphql.ts
@@ -5,6 +5,10 @@ export interface AllUsersQuery {
   allUsers: User[];
 }
 
+export interface UserQuery {
+  User: User;
+}
+
 export const ALL_USERS_QUERY = gql`
   query AllUserQuery($idToExclude: ID!) {
     allUsers(
@@ -18,5 +22,15 @@ export const ALL_USERS_QUERY = gql`
       email
       createdAt
     }
+  }
+`;
+
+export const GET_USER_BY_ID_QUERY = gql`
+  query GetUserByIdQuery($userId: ID!) {
+  User(id: $userId) {
+    id
+    name
+    email
+    createdAt
   }
 `;
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -3,7 +3,7 @@ import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../models/user.model';
-import { ALL_USERS_QUERY, AllUsersQuery } from './user.graphql';
+import { ALL_USERS_QUERY, AllUsersQuery, GET_USER_BY_ID_QUERY, UserQuery } from './user.graphql';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +20,15 @@ export class UserService {
       variables: {idToExclude}
     }).pipe(
       map(res => res.data.allUsers)
+    );
+  }
+
+  public getUserById(id: string): Observable<User> {
+    return this.apollo.query<UserQuery>({
+      query: GET_USER_BY_ID_QUERY,
+      variables: {userId: id}
+    }).pipe(
+      map(res => res.data.User)
     );
   }
 }
